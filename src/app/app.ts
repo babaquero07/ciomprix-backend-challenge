61ac45b8b26b2bf084990b8558b601b42d743207
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-03-08 09:39:02 -0500

    feat(config): server config

---
+++ src/app/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import { config } from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+// import routes from './routes'
+
+config();
+
+const app = express();
+
+// Middlewares
+app.use(cors({ credentials: true }));
+app.use(express.json());
+
+// Logger TODO: Remove in production
+app.use(morgan("dev"));
+
+// app.use('/api', routes)
+
+export default app;
