4943320e75ab9b4e3a84b80b7eb825931fa9ce88
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-03-08 10:21:31 -0500

    Add AuthService and related utility functions

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Response } from "express";
+
+import { COOKIE_NAME } from "../../utils/constants";
+import { createToken } from "../../utils/token-manager";
+
+export class AuthService {
+  static clearCookie = (res: Response) => {
+    res.clearCookie(COOKIE_NAME, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      signed: true,
+    });
+  };
+
+  static createAndSendToken = (
+    res: Response,
+    userId: string,
+    email: string,
+    role: string
+  ) => {
+    const expiresInMinutes = 5;
+    const token = createToken(userId, email, role, `${expiresInMinutes}m`);
+
+    const expires = new Date();
+    // Convertir minutos a milisegundos y agregar a la fecha actual
+    expires.setTime(expires.getTime() + expiresInMinutes * 60 * 1000);
+
+    // Set the new cookie
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+  };
+}
