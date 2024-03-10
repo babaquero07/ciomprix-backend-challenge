fd1fbd4cd4a92c5512b4a41ee2dde6fa1bbce5d4
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-03-09 22:26:35 -0500

    feat(swagger): add Swagger documentation support

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import { config } from "dotenv";
+import { swaggerDocs } from "../documentation/swagger";
 
 config();
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}/api`);
+  swaggerDocs(app, PORT);
 });
 
 server.on("error", console.error);
---
+++ src/documentation/swagger.ts
@@ -0,0 +1,38 @@
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+// Metadata info about our API
+
+const options = {
+  definition: {
+    info: {
+      title: "Ciomprix backend challenge",
+      version: "1.0.0",
+      description: "API Documentation",
+      contact: {
+        name: "Alexander Baquero",
+        url: "https://github.com/babaquero07",
+      },
+    },
+    servers: [
+      { url: "http://localhost:8080", description: "Development server" },
+    ],
+    basePath: "/api",
+  },
+  apis: ["**/*.ts", "**/**/*.ts"],
+  explore: true,
+};
+
+// Docs in JSON format
+const swaggerSpec = swaggerJsDoc(options);
+
+// Function to setup our docs
+export const swaggerDocs = (app, port) => {
+  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/api/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+
+  console.log(`📑 Docs are available at http://localhost:${port}/api/docs`);
+};
